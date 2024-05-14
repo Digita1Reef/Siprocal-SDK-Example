e955131d749f5ef291105793b977fb43fab44822
elmer <elmer.palacios@digitalreef.com>
2024-05-14 10:40:44 -0600

    Splash Support Added

--- app/src/main/java/com/siprocal/sdkexample/MainActivity.kt
+++ app/src/main/java/com/siprocal/sdkexample/MainActivity.kt
@@ -2,10 +2,14 @@ package com.siprocal.sdkexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.siprocal.sdk.client.SiprocalSDK
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SiprocalSDK.showAvailableAd(this)
+
     }
 }
