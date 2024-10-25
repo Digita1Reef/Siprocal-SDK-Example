5cef14ce1a7a4d83c5f93284c42861740b4fe6a2
elmer <elmer.palacios@digitalreef.com>
2024-10-25 08:26:54 -0600

    SDK updated to 5.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    implementation ("com.digitalreef.phoenix:<variant>:5.2.1")
+    implementation ("com.digitalreef.phoenix:<variant>:5.3.1")
 }
