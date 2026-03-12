ca3b8b195bbea5ac0875a42775b9335b9178d46e
epalacioe <53627439+epalacioe@users.noreply.github.com>
2026-03-12 09:56:11 -0600

    Enable 16 KB native library compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,11 @@ android {
             )
         }
     }
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
