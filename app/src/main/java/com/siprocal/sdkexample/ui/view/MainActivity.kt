22b9d6c1c4a2778c350a1aa164283956c5fe16fa
epalacioe <53627439+epalacioe@users.noreply.github.com>
2026-06-01 13:20:56 -0600

    Refresh SDK status after initialization

--- app/src/main/java/com/siprocal/sdkexample/ui/view/MainActivity.kt
+++ app/src/main/java/com/siprocal/sdkexample/ui/view/MainActivity.kt
@@ -57,8 +57,9 @@ class MainActivity : AppCompatActivity() {
             checkAndRequestNotificationPermission()
         }
 
-        refreshData()
         SiprocalSDK.showAvailableAd(this)
+        refreshData()
+        scheduleSdkStatusRefresh()
     }
 
     private fun checkAndRequestNotificationPermission() {
@@ -93,6 +94,19 @@ class MainActivity : AppCompatActivity() {
             displaySdkData(sdkInfo)
             viewModel.deleteOldNotifications()
             setRefreshing(false)
+        }
+    }
+
+    private fun scheduleSdkStatusRefresh() {
+        lifecycleScope.launch {
+            repeat(6) {
+                delay(10_000)
+                val currentState = binding.stateValue.text.toString()
+                if (!currentState.equals("INITIAL", ignoreCase = true)) {
+                    return@launch
+                }
+                refreshData()
+            }
         }
     }
 
